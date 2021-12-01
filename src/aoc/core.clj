819fc1549ba53e126f48c2196ba9fb839d86b89c
Oskar Kvist <oskar.kvist@gmail.com>
2021-12-01 17:49:34 +0100

    Add day 1 solution

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -1,2 +1,16 @@
 (ns aoc.core
-  (:use oskarkv.utils))
+  (:use oskarkv.utils)
+  (:require [clojure.string :as str]))
+
+(def parse-int #(Integer/parseInt %))
+
+(defn num-increases [window-size values]
+  (->>$ (partition window-size 1 values)
+    (map sum)
+    (map < $ (rest $))
+    (filter #{true})
+    count))
+
+(defn solve-1 []
+  (->> (map parse-int (str/split (slurp "input/1.txt") #"\n"))
+    (num-increases 3)))
