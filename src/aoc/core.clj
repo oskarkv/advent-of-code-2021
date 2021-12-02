a41293c1d7eca3b8cee6482e58636d154728356e
Oskar Kvist <oskar.kvist@gmail.com>
2021-12-02 19:54:59 +0100

    Add day 2 solution

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -2,6 +2,9 @@
   (:use oskarkv.utils)
   (:require [clojure.string :as str]))
 
+(defn read-input [n]
+  (str/split (slurp (str "input/" n ".txt")) #"\n"))
+
 (def parse-int #(Integer/parseInt %))
 
 (defn num-increases [window-size values]
@@ -12,5 +15,28 @@
     count))
 
 (defn solve-1 []
-  (->> (map parse-int (str/split (slurp "input/1.txt") #"\n"))
+  (->> (map parse-int (read-input 1))
     (num-increases 3)))
+
+(defn eventual-pos [coll]
+  (->>$ (group-by first coll)
+    (fmap #(map lastv %))
+    (fmap sum)
+    (assoc $ :depth (- (:down $) (:up $)))))
+
+(defn eventual-pos-with-aim [coll]
+  (reduce (fn [{:keys [forward aim depth] :as m} [dir v]]
+            (if (= dir :forward)
+              (-> (update m :forward + v)
+                (update :depth + (* aim v)))
+              (update m :aim (if (= dir :up) - +) v)))
+          {:forward 0 :aim 0 :depth 0}
+          coll))
+
+(defn solve-2 []
+  (->>$ (read-input 2)
+    (map #(str/split % #"\s"))
+    (map (juxt (comp keyword first) (comp parse-int lastv)))
+    eventual-pos-with-aim
+    ((juxt :forward :depth))
+    (apply *)))
