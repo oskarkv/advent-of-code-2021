a3bbb26bab4d87fc0b4158e23b75d588c3282e85
Oskar Kvist <oskar.kvist@gmail.com>
2021-12-05 04:48:22 +0100

    Add day 4 solution

--- src/aoc/core.clj
+++ src/aoc/core.clj
@@ -2,8 +2,11 @@
   (:use oskarkv.utils)
   (:require [clojure.string :as str]))
 
+(defn read-raw-input [n]
+  (slurp (str "input/" n ".txt")))
+
 (defn read-input [n]
-  (str/split (slurp (str "input/" n ".txt")) #"\n"))
+  (str/split (read-raw-input n) #"\n"))
 
 (defn parse-int
   ([x] (Integer/parseInt x))
@@ -78,3 +81,27 @@
 
 (day-3-solver solve-3-2
   ((apply juxt (map find-by-criterion [most-common least-common]))))
+
+(defn winner? [drawn board]
+  (let [rows (partition 5 board)
+        cols (transpose rows)]
+    (when (some identity (map #(every? (set drawn) %) (concat rows cols)))
+      [drawn board])))
+
+(defn winners [drawn boards]
+  ((fn f [n left]
+     (let [w? #(winner? (take n drawn) %)]
+       (when (seq left)
+         (lazy-cat (keep w? left) (f (inc n) (remove w? left))))))
+   5 boards))
+
+(defn score [[drawn board]]
+  (* (last drawn) (sum (remove (set drawn) board))))
+
+(defn solve-4 []
+  (let [[drawn boards] (str/split (read-raw-input 4) #"\n" 2)
+        drawn (map parse-int (str/split drawn #","))
+        boards (->> (str/split (str/trim boards) #"\s+")
+                 (map parse-int)
+                 (partition 25))]
+    (map score ((juxt first last) (winners drawn boards)))))
